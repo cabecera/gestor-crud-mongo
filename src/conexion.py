43a944495b2ffdd83e55507c0f51032d7af27228
Carlos Ignacio <whitebownitro@gmail.com>
2025-07-08 11:32:07 -0400

    se anade un try except a conexion por si se produce un error al conectar con la base de datos

--- src/conexion.py
+++ src/conexion.py
@@ -1,6 +1,12 @@
 from pymongo import MongoClient
 
-def conectar():
-    cliente = MongoClient("mongodb://localhost:27017/")
-    bd = cliente["proyecto_mongo"]
-    return bd
+
+try:
+    def conectar():
+        cliente = MongoClient("mongodb://localhost:27017/")
+        bd = cliente["proyecto_mongo"]
+        return bd
+
+except Exception as ex:
+    print(f"Error al conectar a la base de datos: {ex}")
+    exit()
